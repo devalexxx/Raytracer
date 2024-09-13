335cd2a0f443eab0cd40a3755c9b7dd70a7e1364
alspin8 <46360108+alspin8@users.noreply.github.com>
2024-09-13 07:41:22 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Alex on 13/09/2024.
+//
+
+#include <cstdlib>
+
+int main(int /* argc */, char** /* argv */)
+{
+    return EXIT_SUCCESS;
+}
---
+++ xmake.lua
@@ -0,0 +1,14 @@
+set_project("Raytracer")
+set_version("0.0.1")
+
+set_languages("c++23")
+set_warnings("allextra")
+
+add_requires("stb", "glm")
+
+target("Raytracer")
+    set_kind("binary")
+
+    add_files("src/*.cpp")
+
+    add_packages("stb", "glm")
