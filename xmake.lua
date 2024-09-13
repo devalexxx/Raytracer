417fe699ebdcf7df4dcd7232289dc580a41bc7e9
alspin8 <46360108+alspin8@users.noreply.github.com>
2024-09-13 11:25:12 +0200

    Add fmt package

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,11 @@ set_version("0.0.1")
 set_languages("c++23")
 set_warnings("allextra")
 
-add_requires("stb", "glm")
+add_requires("stb", "glm", "fmt")
 
 target("Raytracer")
     set_kind("binary")
 
     add_files("src/*.cpp")
 
-    add_packages("stb", "glm")
+    add_packages("stb", "glm", "fmt")
